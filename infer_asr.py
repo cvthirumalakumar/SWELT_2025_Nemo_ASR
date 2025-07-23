d5239d8c61706ad696e7f76ea3bb8f2da3f1badf
Chowdam Venkata Thirumala Kumar <kumarcvt55@gmail.com>
2025-07-23 14:53:41 +0530

    Update infer_asr.py

--- infer_asr.py
+++ infer_asr.py
@@ -1,3 +1,3 @@
 import nemo.collections.asr as nemo_asr
-asr_model = nemo_asr.models.ASRModel.from_pretrained("stt_en_fastconformer_transducer_large")
+asr_model = nemo_asr.models.ASRModel.restore_from("path/to/trained/model")
 transcript = asr_model.transcribe(["path/to/audio_file.wav"])[0].text
